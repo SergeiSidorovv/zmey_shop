f2b32abaa5657fd73b5c0ab48497eba9b26aa6b4
Sergei Sidorov <s.sidorov1990@mail.ru>
2024-02-06 15:35:30 +0300

    Create: Model CategoryGoods has been created
    Model CategoryGoods has been created in database. And category_id has
    been created to the Goods model.
    
    Changes to be committed:
    
    new file:zmey_shop/goods/migrations/0006_categorygoods_goods_category.py
    modified:zmey_shop/goods/models.py

--- zmey_shop/goods/models.py
+++ zmey_shop/goods/models.py
@@ -6,12 +6,21 @@ class Goods(models.Model):
     name = models.CharField(max_length=60, verbose_name="Имя")
     slug = models.SlugField(max_length=60, unique=True, verbose_name="URL")
     article = models.IntegerField(unique=True, null=True, verbose_name="Артикль")
-    description = models.TextField(max_length=5000, null=True, verbose_name="Описание товара")
+    description = models.TextField(
+        max_length=5000, null=True, verbose_name="Описание товара"
+    )
     yarn = models.CharField(max_length=100, null=True, verbose_name="Название пряжи")
     composition = models.CharField(max_length=100, null=True, verbose_name="Состав")
-    additional_materials = models.CharField(max_length=100, null=True, verbose_name="Доп. материалы")
+    additional_materials = models.CharField(
+        max_length=100, null=True, verbose_name="Доп. материалы"
+    )
     color = models.CharField(max_length=60, verbose_name="Цвет")
-    main_photo = models.ImageField(upload_to='main_photo', null=True , verbose_name='Основное_фото')
+    main_photo = models.ImageField(
+        upload_to="main_photo", null=True, verbose_name="Основное_фото"
+    )
+    category = models.ForeignKey(
+        "CategoryGoods", on_delete=models.PROTECT, null=True, verbose_name="категории"
+    )
 
     def __str__(self):
         return self.name
@@ -24,3 +33,15 @@ class Goods(models.Model):
         verbose_name_plural = "Одежды"
         indexes = [models.Index(fields=["slug"])]
         ordering = ["id"]
+
+
+class CategoryGoods(models.Model):
+    name = models.CharField(max_length=60, verbose_name="Категория")
+    slug = models.SlugField(max_length=60, unique=True, verbose_name="URL-категории")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
