e7622786261f428b54f81dfcaa2deaff4b00f4b2
Sergei Sidorov <s.sidorov1990@mail.ru>
2024-04-10 14:50:53 +0300

    Add:Added email configuration for sending mail
    Changes to be committed:
    	modified:   zmey_shop/zmey_shop/settings.py

--- zmey_shop/zmey_shop/settings.py
+++ zmey_shop/zmey_shop/settings.py
@@ -216,4 +216,10 @@ LOGOUT_REDIRECT_URL = 'goods'
 LOGIN_URL = 'users:login'
 
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
